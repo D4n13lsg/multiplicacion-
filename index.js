3b566c7de75af782620ccff910dd27512e40d360
danielsg <101757213+D4n13lsg@users.noreply.github.com>
2022-07-01 13:45:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//NOMBRE:Daniel felipe sanchez guevara
+//ultima modificacion 22/06/2022
+
+//es una estructura de control de la mayoría de los lenguajes de programación estructurados cuyo propósito es ejecutar un bloque de código y repetir la ejecución mientras se cumpla cierta condición expresada en la cláusula while.
+var valid,num,i;
+do {
+    do {
+        multi='tabla de multiplicar';
+        num=prompt('digite tabla')
+        if(num==""){
+            num=1;
+        }
+    } while (isNaN(num)||num<1);
+    for(i=1; i<=10; i++){
+    multi=multi+'\n'+(num+' x '+i+' = '+num*i)
+    }
+    alert(multi)
+    dato=confirm('desea otra tabla')
+} while (dato===true);
+
